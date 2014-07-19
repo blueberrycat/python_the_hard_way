d555339e76c4330e810ee08cabbecae84fd3dc8d
Anna Luberg <anna@luberg.me>
2014-07-18 19:35:29 -0700

    exercise 10

---
+++ ex10.py
@@ -0,0 +1,23 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "i'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+shaking_crab = """
+we will eat:
+\tFish soup
+\t\tSeaweed
+\nAnd for desert %s and %s 
+""" % ('chocolate', 'apples')
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+print shaking_crab
