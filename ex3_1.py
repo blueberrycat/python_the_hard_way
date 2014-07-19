f8b1ac51955940dbabd5736e4663b2db6815f8c6
Anna Luberg <anna@luberg.me>
2014-07-18 18:46:27 -0700

    First commit

---
+++ ex3_1.py
@@ -0,0 +1,13 @@
+print "How many candies does Anya ate?"
+anya_candies = 3+2+7
+print "Anya's candies", anya_candies
+
+
+
+print "How many apples does Alex ate?"
+alex_apples = 10+3
+print "Alex's apples", alex_apples
+
+print "How many items they ate togeather?"
+
+print anya_candies+alex_apples
