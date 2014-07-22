679246934f4c54b7241b35847a7dcb66212fca7b
Anna Luberg <anna@luberg.me>
2014-07-22 16:37:39 -0700

    Exercise 11

---
+++ ex11.py
@@ -0,0 +1,10 @@
+print "How old are you?"
+p = '# '
+age = raw_input(p)
+print "How tall are you?"
+height = raw_input(p)
+print "How much do you weigh?"
+weight = raw_input(p)
+
+print "So, you are %s old, %s tall and %s heavy." % (age, height, weight)
+
---
+++ ex11_1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+print "I like to SPELL"
+heart = "❤ "
+
+letter1 = raw_input(heart)
+letter2 = raw_input(heart)
+letter3 = raw_input(heart)
+letter4 = raw_input(heart)
+letter5 = raw_input(heart)
+
+print "%s%s%s%s%s" % (letter1, letter2, letter3,letter4, letter5)
